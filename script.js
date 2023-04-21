116563408f4d50e98496f652711fe7333a723eb1
Lucas Generozo <lucasgenerozo@outlook.com>
2023-04-21 14:01:29 -0300

    Started creating a purchase screen

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// TAMANHO DA FONTE
 const btnTamanho = document.getElementsByClassName("btnTamanho");
 let fonte = 16;
 
@@ -30,6 +31,8 @@ function muda(tipo) {
         document.body.style.fontSize = `${fonte}px`
 }
 
+
+//VER MAIS/MENOS FLORES
 const btnVerMais = document.getElementById("btnVerMais");
 const linhaSaibaMais = document.getElementsByClassName("verMais");
 let visible = false;
@@ -53,8 +56,55 @@ function show() {
 
     //aqui eu to me aproveitando e muito do fato do js interpretar verdadeiro como 1 e falso como 0
     btnVerMais.value = txts[Number(visible)];
+
+    window.scrollTo(linhaSaibaMais[1]);
 }
 
 btnVerMais.addEventListener('click', () => {
     show();
 });
+
+
+//TELA DE COMPRA
+
+
+const btnFecha = document.getElementById("btnFecha");
+const scrCompra = document.getElementById("scr-compra");
+const btnsFlor = document.getElementsByClassName("card");
+
+function openScr() {
+    scrCompra.style.display = 'flex'
+}
+
+for (el of btnsFlor) {
+    el.addEventListener('click', () => {
+        openScr();
+    })
+}
+
+btnFecha.addEventListener('click', () => {
+    scrCompra.style.display = 'none'
+})
+
+
+//aumentar quantidade
+const qtd = document.getElementById("qntProd");
+const btnQtds = document.getElementsByClassName("btnQtds");
+let qntProd = 0;
+
+function alterQtd(tipo) {
+    if (tipo == 0) {
+        qntProd == 0 ? null : qntProd--;
+    } else {
+        qntProd++;
+    }
+    qtd.innerHTML = qntProd;
+}
+
+//btn de menos
+for (let i = 0; i < 2; i++) {
+    btnQtds[i].addEventListener('click', () => {
+        alterQtd(i)
+    })
+}
+
